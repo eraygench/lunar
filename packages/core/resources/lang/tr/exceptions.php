e36bd654eb3ee745488bb67f50d1cf3629341216
Eray Genç <eraygench@gmail.com>
2024-08-26 19:32:42 +0300

    Translate the core package into Turkish

---
+++ packages/core/resources/lang/tr/exceptions.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'non_purchasable_item' => '":class" modeli, satın alınabilir arayüzünü uygulamamaktadır.',
+    'cart_line_id_mismatch' => 'Bu sepet satırı bu sepete ait değil',
+    'invalid_cart_line_quantity' => 'Miktarın en az “1” olması bekleniyordu, “:quantity” bulundu.',
+    'maximum_cart_line_quantity' => 'Miktar, :quantity değerini geçemez.',
+    'carts.invalid_action' => 'Sepet işlemi geçersizdi',
+    'carts.shipping_missing' => 'Bir teslimat adresi gereklidir',
+    'carts.billing_missing' => 'Bir fatura adresi gereklidir',
+    'carts.billing_incomplete' => 'Fatura adresi eksik',
+    'carts.order_exists' => 'Bu sepet için bir sipariş zaten mevcut',
+    'carts.shipping_option_missing' => 'Gönderim Seçeneği Eksik',
+    'missing_currency_price' => '":currency" para birimi için fiyat mevcut değil',
+    'minimum_quantity' => 'En az :quantity ürün eklemelisiniz.',
+    'quantity_increment' => 'Miktar :quantity, :increment\'ın katları halinde olmalıdır',
+    'fieldtype_missing' => '":class" alan türü mevcut değil',
+    'invalid_fieldtype' => '“:class” sınıfı FieldType arayüzünü uygulamıyor.',
+    'discounts.invalid_type' => 'Koleksiyon yalnızca ":expected" içerebilir, ":actual" bulundu', //TODO
+    'disallow_multiple_cart_orders' => 'Sepetlerin yalnızca bir siparişi olabilir.',
+];
